217da872643ccf4cd938c94fc36cef37551db639
Fayo08 <oyendefavour12@gmail.com>
2024-06-10 00:01:14 -0600

    formatted document

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,31 +1,37 @@
-import playIcon from '../../assets/Logo/BrainFlix-logo.svg';
-import uploadIcon from '../../assets/Icons/upload.svg';
-import avatar from '../../assets/Images/Mohan-muruge.jpg';
+import playIcon from "../../assets/Logo/BrainFlix-logo.svg";
+import uploadIcon from "../../assets/Icons/upload.svg";
+import avatar from "../../assets/Images/Mohan-muruge.jpg";
 
-
-
-
-import './Header.scss'
-
+import "./Header.scss";
 
 const Header = () => {
-    return (
-        <div className='Header'>
-            <img src={playIcon} alt="logo" className="Header__logo" />
-            <div className='Header__content'>
-            <div className='Header__container'>
-                <input type="search" name="search" id="seardID"  placeholder='Search' className='Header__searchInput'/>
-                <label htmlFor="search" className='Search button'></label>
+  return (
+    <div className="Header">
+      <img src={playIcon} alt="logo" className="Header__logo" />
+      <div className="Header__content">
+        <div className="Header__container">
+          <input
+            type="search"
+            name="search"
+            id="seardID"
+            placeholder="Search"
+            className="Header__searchInput"
+          />
+          <label htmlFor="search" className="Search button"></label>
 
-                <button className='Header__upload' ><span><img src= {uploadIcon} alt="upload icon" /></span> UPLOAD</button>
-                </div>
-                <div className='Header__avatar--container'>
-                    <img src= {avatar} alt="Avatar" className='Header__avatar' />
-                </div>
-                
-                </div>
-            </div>
-    );
+          <button className="Header__upload">
+            <span>
+              <img src={uploadIcon} alt="upload icon" />
+            </span>{" "}
+            UPLOAD
+          </button>
+        </div>
+        <div className="Header__avatar--container">
+          <img src={avatar} alt="Avatar" className="Header__avatar" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/components/VideoContainer/VideoContainer.jsx
+++ src/components/VideoContainer/VideoContainer.jsx
@@ -1,64 +1,52 @@
 import VideoItem from "../VideoItem/VideoItem";
-import VideoDetails from "../VideoDetails/VideoDetails"
-import './VideoContainer.scss'
-import { useState } from 'react'
+import VideoDetails from "../VideoDetails/VideoDetails";
+import "./VideoContainer.scss";
+import { useState } from "react";
 import CommentItem from "../Comment Item/CommentItem";
 
+function VideoContainer({ videoDetailsData }) {
+  if (!Array.isArray(videoDetailsData)) {
+    return <div>Invalid data format</div>;
+  }
 
-function VideoContainer({videoDetailsData}) {
+  const [mainVideo, setmainVideo] = useState(videoDetailsData);
 
-    if (!Array.isArray(videoDetailsData)) {
-        return <div>Invalid data format</div>;
-      }
+  const selectVideo = (videoTitle) => {
+    const videoToSelect = videoDetailsData.find((video) => {
+      return video.title === videoTitle;
+    });
 
-
-      const [mainVideo, setmainVideo] = useState(videoDetailsData)
-
-      const selectVideo = (videoTitle) => {
-     
-    
-        const videoToSelect = videoDetailsData.find((video) => {
-          return video.title === videoTitle;
-        });
-       
-        const filteredVideos = mainVideo.filter((video)=> video.title !== videoToSelect.title)
-        
-        setmainVideo([videoToSelect, ...filteredVideos]);
-      }
-
-
-
-const firstVideo = mainVideo.slice(0, 1)[0]
-const restVideos = mainVideo.slice(1)
-
-    return (
-        <article className="article">
-            <VideoDetails 
-            {...firstVideo}
-             />
-
-            <h3 className="article__subheader" >Next Videos</h3>
-            <ul className="article__list">
-                { restVideos.map ((video) => {
-
-                    return (
-                      <VideoItem
-                      selectVideo = {selectVideo}
-
-                      key= {video.id} 
-                      title= {video.title}
-                      video= {video.video}
-                      channel= {video.channel}
-                      poster = {video.image}
-
-                      />
-                    )
-
-                })}
-
-            </ul>
-        </article>
+    const filteredVideos = mainVideo.filter(
+      (video) => video.title !== videoToSelect.title
     );
+
+    setmainVideo([videoToSelect, ...filteredVideos]);
+  };
+
+  const firstVideo = mainVideo.slice(0, 1)[0];
+  const restVideos = mainVideo.slice(1);
+
+  return (
+    <article className="article">
+      <VideoDetails {...firstVideo} />
+
+      <h3 className="article__subheader">Next Videos</h3>
+      <ul className="article__list">
+        {restVideos.map((video) => {
+          return (
+            <VideoItem
+              selectVideo={selectVideo}
+              key={video.id}
+              title={video.title}
+              video={video.video}
+              channel={video.channel}
+              poster={video.image}
+            />
+          );
+        })}
+      </ul>
+    </article>
+  );
 }
 
 export default VideoContainer;
